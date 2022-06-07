c9cedecb13e326a45fa9239734a29dcc0450a381
hamidrezasafayee <hamidrezasfy@gmail.com>
2022-06-07 14:03:58 +0430

    Create a request class.

---
+++ app/core/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Core;
+
+class Request
+{
+    public static function all()
+    {
+        $request = [];
+
+        if (count($_POST) > 0)
+            $request['post'] = $_POST;
+
+        if (count($_GET) > 0)
+            $request['get'] = $_GET;
+
+        if (count($_FILES) > 0)
+            $request['file'] = $_FILES;
+
+        return json_decode(json_encode($request));
+    }
+
+    public static function get($key)
+    {
+        $request = self::all();
+        return $request->$key;
+    }
+
+    public static function has($key)
+    {
+        if (array_key_exists($key, self::all()))
+            return true;
+
+        return false;
+    }
+
+    public static function old($key, $value)
+    {
+        $data = self::all();
+        return isset($data->$key->$value) ? $data->$key->$value : '';
+    }
+}
